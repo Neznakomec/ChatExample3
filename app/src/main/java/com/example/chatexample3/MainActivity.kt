db0f9c80d82c71730316ab1feac64a38c41ba514
Neznakomec <misha-ershov@ya.ru>
2018-12-27 00:20:28 +0300

    Handle User Sign-In

--- app/src/main/java/com/example/chatexample3/MainActivity.kt
+++ app/src/main/java/com/example/chatexample3/MainActivity.kt
@@ -1,48 +1,77 @@
 package com.example.chatexample3
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+
+
 
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val SIGN_IN_REQUEST_CODE: Int = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //testFirebase(getString(R.string.test_mail), getString(R.string.test_pass))
-        checkAuthentification(getString(R.string.test_mail), getString(R.string.test_pass))
-    }
-
-    fun testFirebase(emailAddress: String, password: String) {
-        FirebaseAuth.getInstance()
-            .createUserWithEmailAndPassword(emailAddress, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // User registered successfully
-                    Log.d("TEST", task.toString());
-                }
-            }
-    }
-
-    private fun checkAuthentification(emailAddress: String, password: String) {
-        val auth = FirebaseAuth.getInstance()
-        if (auth.currentUser != null) {
-            // Already signed in
-            // Do nothing
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Start sign in/sign up activity
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .build(),
+                SIGN_IN_REQUEST_CODE
+            );
         } else {
-            auth.signInWithEmailAndPassword(emailAddress, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        // User signed in successfully
-                    }
-                }
+            // User is already signed in. Therefore, display
+            // a welcome Toast
+            Toast.makeText(this,
+                "Welcome " + FirebaseAuth.getInstance()
+                    ?.getCurrentUser()
+                    ?.getDisplayName(),
+                Toast.LENGTH_LONG)
+                .show();
+
+            // Load chat room contents
+            displayChatMessages();
         }
     }
 
-    private fun testSignOut() {
-        FirebaseAuth.getInstance().signOut()
+    private fun displayChatMessages() {
+
+    }
+
+    override fun onActivityResult(
+        requestCode: Int, resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(
+                    this,
+                    "Successfully signed in. Welcome!",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+                displayChatMessages()
+            } else {
+                Toast.makeText(
+                    this,
+                    "We couldn't sign you in. Please try again later.",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+
+                // Close the app
+                finish()
+            }
+        }
+
     }
 }
