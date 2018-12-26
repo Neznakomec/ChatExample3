d851a55eda4c1b4d2b485875063122d54881592d
Neznakomec <misha-ershov@ya.ru>
2018-12-27 01:15:22 +0300

    fix layout error and nomethod error

--- app/src/main/java/com/example/chatexample3/MainActivity.kt
+++ app/src/main/java/com/example/chatexample3/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         if(FirebaseAuth.getInstance().getCurrentUser() == null) {
             // Start sign in/sign up activity
             startActivityForResult(
